4119fde8b464875505e9dbee7f0533126be867db
luongnv1 <luongnv1@kaopiz.com>
2023-12-26 15:08:24 +0700

    logout user

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -21,6 +21,29 @@ router.post('/users', async (req, res) => {
     // })
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.status(200).send()
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
