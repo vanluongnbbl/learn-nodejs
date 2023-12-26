466a5dd130d6fe67c37a1cb505b24b065edc3ce8
luongnv1 <luongnv1@kaopiz.com>
2023-12-26 10:10:45 +0700

    middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests qre disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+
+app.use((req, res, next) => {
+    res.status(503).send('Website is maintaining...')
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
