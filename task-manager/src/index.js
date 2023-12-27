7e2d1f4523d0d8d14e6723e2afba31f5930f1472
luongnv1 <luongnv1@kaopiz.com>
2023-12-27 11:59:49 +0700

    working with timestamp

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -19,19 +19,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port );
 })
-
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('658b888d9322d9b333a78325')
-    // await task.populate('owner')
-    // console.log('tsss',task.owner)
-
-    const user = await User.findById('658aa5835c650eece0b489e9')
-    await user.populate('tasks')
-    console.log('user', user.tasks)
-}
-
-main()
