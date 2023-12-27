90dba8d05225dc7709132ba1f5195f67858f468c
luongnv1 <luongnv1@kaopiz.com>
2023-12-27 16:37:42 +0700

    validating file uploads

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -96,7 +96,20 @@ router.delete('/users/me', auth, async (req, res) => {
 
 
 const upload = multer ({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('File must be a jpg, jpeg or png'))
+        }
+
+        // if (!file.originalname.match(/\.(doc|docx)$/)) {
+        //     return cb(new Error('please upload a Word document'))
+        // }
+        cb(undefined, true)
+    }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
