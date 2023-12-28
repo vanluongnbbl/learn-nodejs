e37811486946de2976376e81e638c8e07d31c774
luongnv1 <luongnv1@kaopiz.com>
2023-12-28 09:05:37 +0700

    handling express error

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,14 +8,30 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-// const multer = require('multer')
-// const upload = multer ({
-//      dest: 'images'
-// })
+const multer = require('multer')
+const upload = multer ({
+     dest: 'images',
+     limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('File must be a jpg, jpeg or png'))
+        }
 
-// app.post('/upload', upload.single('upload'), (req, res) => {
-//     res.send()
-// }) 
+        // if (!file.originalname.match(/\.(doc|docx)$/)) {
+        //     return cb(new Error('please upload a Word document'))
+        // }
+        cb(undefined, true)
+    }
+})
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+}) 
+
 
 app.use(express.json())
 app.use(userRouter)
