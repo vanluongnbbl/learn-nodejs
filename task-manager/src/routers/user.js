5acce7de7bed049fa26c17c179deed47d5d43571
luongnv1 <luongnv1@kaopiz.com>
2023-12-28 13:24:44 +0700

    auto cropping and formartting image

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = new express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/user')
+const sharp = require('sharp')
 const multer = require('multer')
 
 router.post('/users', async (req, res) => {
@@ -114,7 +115,10 @@ const upload = multer ({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+
+
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -137,7 +141,7 @@ router.get('/users/:id/avatar', async (req, res) => {
             throw new Error()
         }
 
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
 
     } catch (error) {
