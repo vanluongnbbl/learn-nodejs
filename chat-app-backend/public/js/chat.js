2a6ba0a0fbc3e31596f181ab6678f31a29ab2a31
luongnv1 <luongnv1@kaopiz.com>
2024-01-04 14:35:13 +0700

    send locaiton

--- chat-app-backend/public/js/chat.js
+++ chat-app-backend/public/js/chat.js
@@ -1,1 +1,47 @@
-io()
+const socket = io()
+
+socket.on('countUpdated', (count = 0) => {
+    console.log('The count has been updated!', count)
+})
+
+
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked')
+    socket.emit('increment')
+})
+
+
+socket.on('message', (message) => {
+  if (typeof message === 'function') return
+  console.log(message)
+})
+
+const form = document.getElementById('form');
+const input = document.getElementById('inputChat');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.')
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('send-location', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  })
+})
+
+socket.on('location', (location) => {
+  if (typeof location === 'function') return
+  console.log(location)
+})
--- chat-app-backend/src/index.js
+++ chat-app-backend/src/index.js
@@ -1,21 +1,48 @@
 const express = require('express')
 const path = require('path')
-const cors = require("cors");
+// const cors = require("cors");
 const http = require('http')
 const socketIo = require('socket.io')
 
 const app = express()
-const port = process.env.PORT || 8000
+const server = http.createServer(app)
+const io = socketIo(server)
 
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-const server = http.createServer(app)
-const io = socketIo(server)
-app.use(cors());
+// app.use(cors());
 
+console.log('New Websocket connection')
+let count = 0
 io.on('connection', (socket) => {
-    console.log('New Websocket connection')
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        io.emit('countUpdated', count)
+    })
+    
+    //  chat
+    socket.broadcast.emit('message', 'A new user has joined!')
+    socket.emit('message', message => {
+        console.log(message)
+    })
+    socket.on('chat message', (msg) => {
+        io.emit('message', msg)
+    })
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
+
+
+    // location
+    socket.on('send-location', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+
+    socket.broadcast.emit('location', 'new locaiton')
 }) 
 
 server.listen(port, () => {
